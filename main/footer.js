3f85b6f9f0e9d9e81e7c95a0d88a212fa5cf5832
Jonatan Antoni <jonatan.antoni@arm.com>
2023-03-28 18:33:44 +0200

    Initial contribution

---
+++ main/footer.js
@@ -0,0 +1,7 @@
+function writeHeader() {
+    document.write('Version 0.0.0-dirty1');
+};
+
+function writeFooter()  {
+    document.write('Generated on Tue Mar 28 2023 16:21:27 for PROJECT_NAME           = CMSIS-DAP 0.0.0-dirty1+geaa1bb4. Copyright &copy; 2023 Arm Limited (or its affiliates). All rights reserved.');
+};
